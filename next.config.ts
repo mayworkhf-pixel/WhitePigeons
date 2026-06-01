dcd4ffc4864375663a63f0ec7de3b06c937ab99b
Max <max@example.com>
2026-06-02 05:23:59 +0530

    Configure for static HTML export on Firebase Spark plan

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Static exports do not support Next.js dynamic image optimization
+  }
 };
 
 export default nextConfig;
